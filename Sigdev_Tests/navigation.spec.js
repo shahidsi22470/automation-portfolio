392df69741c33bdc470bae55bec65db717387def
shahid.2i22470 <shahid.si22470@gmail.com>
2026-03-22 01:20:25 +0000

    Add navigation panel tests 4

--- Sigdev_Tests/navigation.spec.js
+++ Sigdev_Tests/navigation.spec.js
@@ -1,5 +1,5 @@
 import { expect, test } from "@playwright/test";
-import { LoginPage } from "../pages/loginPage.js";
+import { LoginPage } from "../pages/LoginPage.js";
 import users from '../testdata/users.json' assert {type: 'json'};
 import { NavigationPanel } from "../pages/NavigationPanel.js";
 
